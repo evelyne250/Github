03cb74b3ff18f102d3b1141501f40570415d5949
pascaline <passyish5@gmail.com>
2019-08-29 12:33:40 +0200

    added elements to user.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../user';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  @Input() user: User;
   constructor() { }
 
   ngOnInit() {
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
+// export class User {
+//     constructor(public id:number, public login:any, public company:string, public location:string, public bio:string, public public_repos: number, public followers: number, public following: number, public created_at: Date, public updated_at: Date ){}
+// }
+
 export class User {
+    constructor(
+        public user: any,
+        public repos: any,
+        public userName: string
+    ) { }
 }
+
